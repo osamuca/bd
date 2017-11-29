569c2efd2cc1cdef7af6f7224b87dace23301b9e
osamuca <sfverneck@gmail.com>
2017-11-28 23:05:11 -0200

    fix(decodificacao/codificacao): padronizado os arquivos.

--- codificacao.js
+++ codificacao.js
@@ -2,9 +2,9 @@ const mensagem = prompt('Digite sua mensagem');
 const cifra = parseInt(prompt('Digite o valor da cifra'));
 
 const codifique = ( mensagem, cifra ) => ( el, i ) => 
-  String.fromCharCode(mensagem.charCodeAt(i) + cifra)
+	String.fromCharCode(mensagem.charCodeAt(i) + cifra);
 
 const mensagemCodificada = mensagem.split('')
-                                    .map( codifique( mensagem, cifra ) )
-
+	.map( codifique( mensagem, cifra ) );
+  
 alert(mensagemCodificada);
--- decodificacao.js
+++ decodificacao.js
@@ -3,9 +3,9 @@ const mensagem = prompt('Digite sua mensagem');
 const cifra = parseInt(prompt('Digite o valor da cifra'));
 
 const decodifique = ( mensagem, cifra ) => ( el, i ) => 
-  String.fromCharCode(mensagem.charCodeAt(i) - cifra)
+	String.fromCharCode(mensagem.charCodeAt(i) - cifra);
 
 const mensagemDecodificada = mensagem.split('')
-                                    .map( decodifique( mensagem, cifra ) )
+	.map( decodifique( mensagem, cifra ) );
 
 alert(mensagemDecodificada);
